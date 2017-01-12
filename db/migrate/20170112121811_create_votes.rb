52031103d53ba2f5cdaf058aed3f5c61c1ceb261
Pat Holland <email": null>
2017-01-12 12:32:48 -0800

    Create votes migration

--- db/migrate/20170112121811_create_votes.rb
+++ db/migrate/20170112121811_create_votes.rb
@@ -1,4 +1,12 @@
 class CreateVotes < ActiveRecord::Migration
   def change
+    create_table :votes do |t|
+      t.integer :user_id, index: true, foreign_key: true
+      t.boolean :value
+      t.string :voteable_type
+      t.integer :voteable_id, index: true, foreign_key: true
+
+      t.timestamps
+    end
   end
 end
