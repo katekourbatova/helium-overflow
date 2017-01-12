9bb0dd390d33ec366c751dc5dd7f2ca18b692ea8
Justin G <email": null>
2017-01-12 15:26:08 -0800

    add validations with specs for questions

--- spec/models/questions_spec.rb
+++ spec/models/questions_spec.rb
@@ -3,6 +3,14 @@ require_relative '../spec_helper'
 describe Question do
   let(:question){Question.new({title: 'Balloon exploded',
                                     body: 'How do you prevent a balloon from exploding?'})}
+  let(:titleless_question){
+    Question.new(body: 'How do you prevent a balloon from exploding?')
+  }
+  let(:bodyless_question){
+    Question.new(title: 'Balloon exploded')
+  }
+  let(:question){Question.new({title: 'Balloon exploded',
+                                    body: 'How do you prevent a balloon from exploding?'})}
   describe "a question can be created" do
     it 'has a title' do
       expect(question.title).to eq('Balloon exploded')
@@ -10,6 +18,16 @@ describe Question do
 
     it 'has a body' do
       expect(question.body).to eq('How do you prevent a balloon from exploding?')
+    end
+  end
+
+  describe "VALIDATIONS" do
+    it 'requires a title' do
+       expect(titleless_question).to be_invalid
+    end
+
+    it 'requires a body' do
+       expect(bodyless_question).to be_invalid
     end
   end
 end
