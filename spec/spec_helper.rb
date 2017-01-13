a3b27dd14d1fceaacab4ca43069d7ba99ae519a0
Pat Holland <email": null>
2017-01-12 16:47:07 -0800

    Update spec helper to include ActiveModel

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,6 +16,7 @@ require 'capybara/rspec'
 RSpec.configure do |config|
   config.include Rack::Test::Methods
   config.include(Shoulda::Matchers::ActiveRecord, type: :model)
+  config.include(Shoulda::Matchers::ActiveModel, type: :model)
 end
 
 def app
