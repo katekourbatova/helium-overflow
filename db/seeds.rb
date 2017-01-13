a32e00642cea33650620b058730db318e2e8cd6c
swwhitaker <email": null>
2017-01-13 15:41:33 -0800

    fixes bugs in comments slice

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,3 +10,18 @@ end
                                 body: Faker::Hipster.paragraph(2, true, 4),
                                 author_id: rand(1..20))
 end
+
+
+50.times do
+  question = Comment.create!(
+                                body: Faker::Hipster.paragraph(2, true, 4),
+                                author_id: rand(1..20), commentable_id: rand(1..20), commentable_type: ["question", "answer"].sample
+                                )
+end
+
+50.times do
+  question = Comment.create!(
+                                body: Faker::Hipster.paragraph(2, true, 4),
+                                author_id: 61, commentable_id: rand(1..20), commentable_type: ["question", "answer"].sample
+                                )
+end
