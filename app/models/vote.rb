de06bc32c0ab4c0139622bb09fb49ace0be5a219
Pat Holland <email": null>
2017-01-12 16:59:28 -0800

    Set up spec to test Vote validations and associations

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,8 +1,9 @@
 class Vote < ActiveRecord::Base
   belongs_to :user
   belongs_to :voteable, polymorphic: true
-
-  validates :user, presence: true
-  validates :voteable, presence: true
+  
   validates :value, presence: true
+  validates :user_id, presence: true
+  validates :voteable_id, presence: true
+  validates :voteable_type, presence: true
 end
