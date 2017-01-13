f8b573f68b5bb55ca3d4209860787d37ae4074a5
Justin G. & Kate Kourbatova <pair+justgrasse.katekourbatova@devbootcamp.com>
2017-01-13 11:53:35 -0800

    include answer spec for association with comments

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -1,9 +1,20 @@
 require_relative '../spec_helper'
 
 describe Answer do
-  let(:answer){Answer.new({body:'Your question has an answer'})}
+  let(:answer){Answer.create!({body:'Your question has an answer', question_id: 1, author_id: 1})}
+  let(:comment){Comment.create!({body:'This is a comment', commentable_type: 'Answer',commentable_id: answer.id})}
 
   it 'has a body' do
     expect(answer.body).to eq('Your question has an answer')
+  end
+
+  describe 'associations' do
+    it 'has many comments' do
+      expect(answer.comments).to include(comment)
+    end
+  end
+
+  describe 'validations' do
+
   end
 end
