cb823cc0aad5cf7d17fa2eaafd25805a78b5945e
Pat Holland <pholls@users.noreply.github.com>
2017-01-13 11:13:56 -0800

    Fix minor ypos

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,49 +1,49 @@
 
 # ROUTES FOR COMMENTS ON QUESTIONS
 
-get '/question/:question_id/comments' do
-  @question = Question.find(params[:question_id])
+get '/questions/:id/comments' do
+  @question = Question.find(params[:id])
   @comments = @question.comments
   erb :'questions/comments/show'
 end
 
-get '/questions/:question_id/comments/new' do
-  @question = Question.find(params[:question_id])
-  erb :'/comments/show'
+get '/questions/:id/comments/new' do
+  @question = Question.find(params[:id])
+  erb :'comments/show'
 end
 
-post '/questions/:question_id/comments' do
+post '/questions/:id/comments' do
   if session_logged_in?
-    @question = Question.find(params[:question_id])
+    @question = Question.find(params[:id])
     @author = session_current_user
     @comment = @question.comments.create(body: params[:comment_body], author_id: @author.id, commentable_id: @question.id, commentable_type: "question" )
   else
     @msgs = "You must be logged in to comment"
   end
-  erb :'/comments/show'
+  erb :'comments/show'
 end
 
 # ROUTES FOR COMMENTS ON ANSWERS
 
-get '/answers/:answer_id/comments' do
-  @answer = Answer.find(params[:answer_id])
+get '/answers/:id/comments' do
+  @answer = Answer.find(params[:id])
   @comments = @answer.comments
-  erb :'answers/'
+  erb :answers
 end
 
-get '/answers/:answer_id/comments/new' do
-  @answer = Answer.find(params[:answer_id])
+get '/answers/:id/comments/new' do
+  @answer = Answer.find(params[:id])
   @comments = @answer.comments
-  erb :'/comments/show'
+  erb :'comments/show'
 end
 
-post '/answers/:answer_id/comments' do
+post '/answers/:id/comments' do
   if session_logged_in?
-    @answer = Answer.find(params[:answer_id])
+    @answer = Answer.find(params[:id])
     @author = session_current_user
     @comment = @answer.comments.create(body: params[:comment_body], author_id: @author.id, commentable_id: @answer.id, commentable_type: "answer" )
   else
     @msgs = "You must be logged in to comment"
   end
-  erb :'/comments/show'
+  erb :'comments/show'
 end
