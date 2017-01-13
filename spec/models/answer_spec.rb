d5c3dceb25a62ee4c0bd1e16f062b1aef602e22b
Justin G. & Kate Kourbatova <pair+justgrasse.katekourbatova@devbootcamp.com>
2017-01-13 12:07:08 -0800

    create validation spec for body in answers

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -1,7 +1,8 @@
 require_relative '../spec_helper'
 
 describe Answer do
-  let(:answer){Answer.create!({body:'Your question has an answer', question_id: 1, author_id: 1})}
+  let(:answer_options) {{body:'Your question has an answer', question_id: 1, author_id: 1}}
+  let(:answer){Answer.create!(answer_options)}
   let(:comment){Comment.create!({body:'This is a comment', commentable_type: 'Answer',commentable_id: answer.id})}
 
   it 'has a body' do
@@ -15,6 +16,9 @@ describe Answer do
   end
 
   describe 'validations' do
-
+    let(:bodyless_answer) {Answer.new(answer_options.except(:body))}
+    it 'requires a body' do
+      expect(bodyless_answer).to be_invalid
+    end
   end
 end
