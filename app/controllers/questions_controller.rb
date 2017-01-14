3798a707e457e82f8977cba6f3d800b0ec6fe47d
Justin G. & Kate Kourbatova <pair+justgrasse.katekourbatova@devbootcamp.com>
2017-01-13 17:00:11 -0800

    add create and new form route for questions

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,13 +1,27 @@
+# All questions - redirects to homepage
 get '/questions' do
   redirect '/'
 end
 
+#homepage
 get '/' do
   @questions = Question.all
   erb :'questions/index'
 end
 
+# a specific question
 get '/questions/:id' do
   @question = Question.find(params[:id])
   erb :'questions/show'
 end
+
+# form for making new question
+get '/questions/new' do
+  erb :'questions/new'
+end
+
+post '/questions' do
+  # ...
+end
+
+# Show New Question Form
--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,7 +1,7 @@
 class Vote < ActiveRecord::Base
   belongs_to :user
   belongs_to :voteable, polymorphic: true
-  
+
   validates :value, presence: true
   validates :user_id, presence: true
   validates :voteable_id, presence: true
