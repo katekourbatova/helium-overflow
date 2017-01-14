115737311aae15b0253abb0df45f4bf1fa8d87af
Justin G. & Kate Kourbatova <pair+justgrasse.katekourbatova@devbootcamp.com>
2017-01-13 20:05:04 -0800

    add answers to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,15 +13,21 @@ end
 
 
 50.times do
-  question = Comment.create!(
+  comment = Comment.create!(
                                 body: Faker::Hipster.paragraph(2, true, 4),
                                 author_id: rand(1..20), commentable_id: rand(1..20), commentable_type: ["question", "answer"].sample
                                 )
 end
 
 50.times do
-  question = Comment.create!(
+  comment = Comment.create!(
                                 body: Faker::Hipster.paragraph(2, true, 4),
                                 author_id: 61, commentable_id: rand(1..20), commentable_type: ["question", "answer"].sample
                                 )
+end
+
+50.times do
+  answer = Answer.create!(  body: Faker::Hipster.paragraph(2, true, 4),
+                            question_id: rand(1..20),
+                            author_id: rand(1..20) )
 end
