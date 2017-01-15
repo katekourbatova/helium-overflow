05ef22967280d4f2857a20ffa54ef79e893589b1
Justin G <email": null>
2017-01-15 11:50:13 -0800

    create association between question and answers

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,6 +2,7 @@ class Question < ActiveRecord::Base
   include Voteable
 
   belongs_to :author, class_name: User
+  has_many :answers
   has_many :comments, :as => :commentable
   has_many :votes, :as => :voteable
 
