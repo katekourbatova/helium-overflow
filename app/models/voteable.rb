71ac3670cd0d6de4cf699067d69aa1ed487dfa49
Pat Holland <email": null>
2017-01-14 17:57:54 -0800

    Delete user's old vote before creating new one

--- app/models/voteable.rb
+++ app/models/voteable.rb
@@ -1,10 +1,20 @@
 module Voteable
   def downvote(user)
+    delete_old_vote(user)
     self.votes.create(value: false, user_id: user.id)
   end
 
   def upvote(user)
+    delete_old_vote(user)
     self.votes.create(value: true, user_id: user.id)
   end
 
+  def get_type
+    self.class.name
+  end
+
+  def delete_old_vote(user)
+    vote = Vote.where(user_id: user.id, voteable_type: get_type, voteable_id: self.id)[0]
+    Vote.destroy(vote.id)
+  end
 end
