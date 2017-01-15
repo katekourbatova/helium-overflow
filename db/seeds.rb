719b8ca8961f9c87536b987d393718f9724f3230
Pat Holland <email": null>
2017-01-14 17:19:22 -0800

    Edit seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,9 @@
+User.all.delete_all
+Question.all.delete_all
+Comment.all.delete_all
+Answer.all.delete_all
+Vote.all.delete_all
+
 20.times do
   user = User.create!( username: Faker::Internet.user_name,
                        email: Faker::Internet.email,
@@ -15,14 +21,14 @@ end
 50.times do
   comment = Comment.create!(
                                 body: Faker::Hipster.paragraph(2, true, 4),
-                                author_id: rand(1..20), commentable_id: rand(1..20), commentable_type: ["question", "answer"].sample
+                                author_id: rand(1..20), commentable_id: rand(1..20), commentable_type: ["Question", "Answer"].sample
                                 )
 end
 
 50.times do
   comment = Comment.create!(
                                 body: Faker::Hipster.paragraph(2, true, 4),
-                                author_id: 61, commentable_id: rand(1..20), commentable_type: ["question", "answer"].sample
+                                author_id: 61, commentable_id: rand(1..20), commentable_type: ["Question", "Answer"].sample
                                 )
 end
 
@@ -30,4 +36,18 @@ end
   answer = Answer.create!(  body: Faker::Hipster.paragraph(2, true, 4),
                             question_id: rand(1..20),
                             author_id: rand(1..20) )
+end
+
+10.times do
+  Answer.each.do |answer|
+    answer.votes.create!(user_id: User.all.sample, value: [true, false].sample)
+  end
+
+  Question.each.do |question|
+    question.votes.create!(user_id: User.all.sample, value: [true, false].sample)
+  end
+
+  Comment.each.do |comment|
+    comment.votes.create!(user_id: User.all.sample, value: [true, false].sample)
+  end
 end
