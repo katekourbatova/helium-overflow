a237857cf242bbf9f6462973c68239827661fbe4
Pat Holland <email": null>
2017-01-14 17:31:06 -0800

    Add validations and polymorphic relationship

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,7 @@
 class Comment < ActiveRecord::Base
-
   belongs_to :author, class_name: User
-  has_many :votes
+  has_many :votes, :as => :voteable
+
+  validates :body, presence: true
+  validates :author, presence: true
 end
