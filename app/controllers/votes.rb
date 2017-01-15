d1469f8aa8fd95e71ba2070d844ee16500f25f6e
Pat Holland <email": null>
2017-01-14 17:31:44 -0800

    Pseudocode necessary steps

--- app/controllers/votes.rb
+++ app/controllers/votes.rb
@@ -1,7 +1,13 @@
 post '/:voteable_type/:id/upvote' do
-
+  # Verify user is logged in
+  # Check if vote exists
+  # If not, upvote
+  # Redirect to page
 end
 
 post '/:voteable_type/:id/downvote' do
-  
+  # Verify user is logged in
+  # Check if vote exists
+  # If not, upvote
+  # Redirect to page
 end
