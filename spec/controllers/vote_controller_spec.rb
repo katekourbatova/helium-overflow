ac719570f7002454d13772a84cfc135c7adfd692
Pat Holland <pholls@users.noreply.github.com>
2017-01-15 15:02:35 -0800

    write specs for vote controller

--- spec/controllers/vote_controller_spec.rb
+++ spec/controllers/vote_controller_spec.rb
@@ -1,1 +1,27 @@
 require 'spec_helper'
+
+describe 'votes controller' do
+  let(:question) {Question.create(title: 'How do I type?', 
+                                  body: 'I have no hands',
+                                  author_id: User.first.id)}
+
+  describe 'upvoting a question' do
+    it 'redirects to the question' do
+      post "questions/{question.id}/upvote"
+      expect(last_response.status).to eq(302)
+    end
+    it 'increases the score of the question' do
+      expect{post "questions/{question.id}/upvote"}.to change{get_score(question)}.by(1)
+    end
+  end
+
+  describe 'downvoting a question' do
+    it 'redirects to the question' do
+      post "questions/{question.id}/downvote"
+      expect(last_response.status).to eq(302)
+    end
+    it 'increases the score of the question' do
+      expect{post "questions/{question.id}/downvote"}.to change{get_score(question)}.by(-11)
+    end
+  end
+end
