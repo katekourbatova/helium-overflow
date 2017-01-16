796d70629636292fbb2d038f83c142f80c37b989
Justin G <email": null>
2017-01-15 16:45:17 -0800

    refactor with added calback function

--- public/js/application.js
+++ public/js/application.js
@@ -1,10 +1,15 @@
 $(document).ready(function() {
   $('#show-answer-form').click(function(event){
     event.preventDefault();
-    $.get($(this).attr('href'), function(response){
-      $('#show-answer-form').hide();
-      $('.question-container').after(response);
-      $('#answer-body').focus();
-    })
+    $.get($(this).attr('href'), showAnswerForm)
   })
+
+///////////////////// CALLBACK FUNCTIONS /////////////////////
+
+function showAnswerForm(response){
+  $('#show-answer-form').hide();
+  $('.question-container').after(response);
+  $('#answer-body').focus();
+}
+
 });
