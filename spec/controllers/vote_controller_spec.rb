45070676b464b02b91a370091916a1d7fcced794
Pat Holland <pholls@users.noreply.github.com>
2017-01-15 16:22:57 -0800

    Specs don't work -- need to stub login/session data

--- spec/controllers/vote_controller_spec.rb
+++ spec/controllers/vote_controller_spec.rb
@@ -4,24 +4,25 @@ describe 'votes controller' do
   let(:question) {Question.create(title: 'How do I type?', 
                                   body: 'I have no hands',
                                   author_id: User.first.id)}
+  let(:session) { {user_id: User.first.id} }
 
   describe 'upvoting a question' do
     it 'redirects to the question' do
-      post "questions/{question.id}/upvote"
+      post "questions/#{question.id}/upvote"
       expect(last_response.status).to eq(302)
     end
     it 'increases the score of the question' do
-      expect{post "questions/{question.id}/upvote"}.to change{get_score(question)}.by(1)
+      expect{post "questions/#{question.id}/upvote"}.to change{question.get_score}.by(1)
     end
   end
 
   describe 'downvoting a question' do
     it 'redirects to the question' do
-      post "questions/{question.id}/downvote"
+      post "questions/#{question.id}/downvote"
       expect(last_response.status).to eq(302)
     end
-    it 'increases the score of the question' do
-      expect{post "questions/{question.id}/downvote"}.to change{get_score(question)}.by(-11)
+    it 'decreases the score of the question' do
+      expect{post "questions/#{question.id}/downvote"}.to change{question.get_score}.by(-1)
     end
   end
 end
